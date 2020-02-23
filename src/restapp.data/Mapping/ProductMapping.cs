5f9106f2bdd85c7edf2961347fad4af2e12cc04e
Diego Malta <diego_malta@hotmail.com>
2020-02-22 23:05:39 -0600

    Initial commit

---
+++ src/restapp.data/Mapping/ProductMapping.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using restapp.Domain.Model;
+
+namespace restapp.Data.Mapping
+{
+    public class ProductMapping : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("CATProductosVenta");
+            builder.HasKey(e => e.ProductId).HasName("iIdProducto");
+            builder.Property(e => e.CategoryId).HasColumnName("iIDGrupoProdVenta");
+            builder.Property(e => e.Name).HasColumnName("sNombre");
+            builder.Property(e => e.Description).HasColumnName("sDescripcion");
+            builder.Property(e => e.Price).HasColumnName("dPrecioVenta");
+            builder.Property(e => e.Color).HasColumnName("sColor");
+            builder.Property(e => e.IsActive).HasColumnName("bActiva");
+            builder.Property(e => e.PreparationPointId).HasColumnName("iIDPuntoEntrega");
+            builder.Property(e => e.SubCategoryId).HasColumnName("iIDSubGrupoProdVenta");
+        }
+    }
+}
---
+++ src/restapp.data/restappDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using restapp.Data.Mapping;
+using restapp.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace restapp.Data
+{
+    public class RestappDbContext : DbContext
+    {
+        public RestappDbContext(DbContextOptions<RestappDbContext> options) : base (options)
+        {
+
+        }
+
+        DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ProductMapping());
+        }
+    }
+}
