15aa5badfc7f7fe5aaff1b5d1d3e7cfe635e86a3
Diego Malta <diego_malta@hotmail.com>
2020-03-01 13:27:18 -0600

    rename some files

---
+++ src/RestApp.Domain/Model/TokenRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestApp.Domain.Model
+{
+    public class TokenRequest
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
